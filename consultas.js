9b3aeff41b4bd5b39bcac006c51ccb3b489a55c7
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2021-09-21 13:39:08 -0300

    Add files via upload

---
+++ consultas.js
@@ -0,0 +1,39 @@
+const {
+    Pool
+} = require('pg')
+const pool = new Pool({
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "skatepark"
+})
+async function ingresarUsuario(email, nombre, pw2, experiencia, especialidad, name) {
+    try {
+        const result = await pool.query(`INSERT INTO skaters(email,nombre,password,anos_experiencia,especialidad,foto,estado)
+         values('${email}','${nombre}','${pw2}',${experiencia},'${especialidad}','${name}',false) RETURNING *;`)
+        const registro = result.rows[0]
+        return registro
+
+    } catch (error) {
+        console.lo(error)
+        return error
+    }
+}
+
+
+async function traerUsuarios() {
+    try {
+
+        const result = await pool.query("SELECT * FROM  skaters")
+        return result.rows
+    } catch (error) {
+        console.log(error)
+        return error;
+    }
+}
+
+
+module.exports = {
+    ingresarUsuario,traerUsuarios,datosUsuario,autenticacion,actualizacion,eliminacion,
+}
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Importaciones
 
 const express = require ( "express" );
-const path = require('path');
+//const path = require('path');
 const app = express();
 const exphbs = require("express-handlebars");
 const expressFileUpload = require("express-fileupload");
-const fs = require('fs');
+//const fs = require('fs');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const secretKey = '1234';
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));  //o bien podria ser (__dirname + "/public");
+app.use(express.static("public"));  // (__dirname + "/public");
 app.use(
   expressFileUpload({
   limits: 5000000,
@@ -45,8 +45,8 @@ app.use(
 app.engine(
     "handlebars",
     exphbs({
-    defaultLayout: "Index",
-    layoutsDir: `${__dirname}/views`,
+    defaultLayout: "Main",
+    layoutsDir: `${__dirname}/views/components`,
     })
     );
 app.set("view engine", "handlebars");
@@ -58,18 +58,69 @@ app.get( "/" ,  (req, res) => {
     res.render("Index");
   });
 
-/* //o podria ser con async como: 
+/* //o podria ser con async, algo como: 
 app.get('/', async (req, res) => {
     const skaters = await traerUsuarios();
     res.render('Index', {skaters}); */
 
+    //registrar usuario en base datos
 app.get("/registro", (req, res) => {
     res.render("Registro")
 });
 
+app.post('/skater', async (req, res) => {
 
-//inicio de sesion
+  let email = req.body.email;
+    let nombre = req.body.nombre;
+    let pw2 = req.body.pw2;
+    let experiencia = req.body.experiencia;
+    let especialidad = req.body.especialidad;
+
+/*   let user_image = req.body.user_image;
+    let r = await InsertarUsuario(nombre,telefono,email,pass,user_image);
+    if (r!=''){
+         res.json({respuesta:"Correcta"})
+    }
+  }); */
+
+    //const skaterDatos = req.body; 
+    //console.log(skaterDatos);
+    let { fotoSkater } = req.files;
+    let {name} = fotoSkater;
+
+
+    fotoSkater.mv(`${__dirname}/public/img/${name}`, async (error, data) => {
+        if (error) res.status(500).send('Error al cargar imagen', error);
+        try {
+            const agregarSkater = await ingresarUsuario(email, nombre, pw2, experiencia, especialidad, name);
+            res.status(201).render("Login");
+            res.status(201).send(agregarSkater);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({
+                error: `Error ${error}`,
+                code: 500
+            })
+        }
+    });
+});
+
+
+
+
+//inicio sesion
 app.get("/login", async (req, res) => {
     res.render("Login")
 });
 
+
+//listado skaters
+app.get("/listado", async (req, res) => {
+    const lista = await traerUsuarios()
+    res.send(lista)
+});
+
+
+app.get("*", (req, res) => {
+    res.send("Ruta invalida")
+})
