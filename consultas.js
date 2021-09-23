4a2df8082dd8849d037d6cb5a57e4cc1c19646ba
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2021-09-23 10:02:24 -0300

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg')
 const pool = new Pool({
     user: 'postgres',
-    password: "mrroboto7",
+    password: "postgres",
     host: "localhost",
     port: 5432,
     database: "skatepark"
